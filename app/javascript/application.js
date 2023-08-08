b4068079b38c2c3d0c26d794ad348e70193eb826
danielJL19 <danieljmnz15@hotmail.com>
2023-08-08 18:05:18 -0400

    feat: se agrego un buscador en lista de inmueble

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,19 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "flowbite";
+//variables
+const room_input = document.getElementById('room_input');
+const room_value = document.getElementById('room_value')
+const bathroom_input = document.getElementById('bathroom_input')
+const bathroom_value = document.getElementById('bathroom_value')
+document.addEventListener("DOMContentLoaded",function(e){
+
+    room_input.addEventListener("input", (event)=>{
+        room_value.textContent = event.target.value
+    })
+
+    bathroom_input.addEventListener('input',(event)=>{
+        bathroom_value.textContent = event.target.value
+    })
+})
+
